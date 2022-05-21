09491ea835df41379e314b1c787027c8f40ee116
Joe Gibbs <joseph.gibbs1188@gmail.com>
2022-05-21 11:50:11 -0500

    Re-doing setup because nothing saved

--- GameCorner/Models/Platform.cs
+++ GameCorner/Models/Platform.cs
@@ -2,5 +2,7 @@
 {
     public class Platform
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- GameCorner/Models/User.cs
+++ GameCorner/Models/User.cs
@@ -1,4 +1,4 @@
-﻿namespace JoesHotDogs.Models
+﻿namespace GameCorner.Models
 {
     public class User
     {
