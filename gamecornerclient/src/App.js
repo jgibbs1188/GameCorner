ecd960abba636eed56c6d1ad5bd22acc6f3ab9b3
Joe Gibbs <joseph.gibbs1188@gmail.com>
2022-06-07 20:45:05 -0500

    able to sign in but there are cors issues now

--- gamecornerclient/src/App.js
+++ gamecornerclient/src/App.js
@@ -1,63 +1,51 @@
-//import React, { useState, useEffect } from "react";
-//import { firebase } from "firebase/compat/app";
-//import "firebase/compat/auth";
-//import Routing from "./Routing";
-import Routes from "./Routing"
+import React, { useState, useEffect } from "react";
+import auth from "./api/auth/apiKeys"
+import checkUserCreatedInDB from "./api/auth/checkUserCreatedInDB";
+import "./App.css";
 import Navbar from "./Components/Navbar";
-//import "./App.css";
+import Routing from "./Routing";
+import Login from "./Views/Login";
 
 function App() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        auth.onAuthStateChanged(async (response) => {
+            if (response) {
+                const userObj = {
+                    uid: response.uid,
+                    fullName: response.displayName,
+                    profilePic: response.photoURL,
+                    username: response.email.split("@")[0],
+                    token: response.accessToken, //you can save the token in an object if you want
+                };
+                setUser(userObj);
+                sessionStorage.setItem("token", response.accessToken);
+                console.log(userObj.uid);
+                checkUserCreatedInDB(response.accessToken);
+            } else {
+                setUser(false);
+
+                //don't forget to clear the token if using sessionStorage!
+                sessionStorage.removeItem("token");
+            }
+        });
+    }, []);
+
     return (
         <div>
-            <Navbar />
-            <Routes />
+            {user ? (
+                <>
+                    <Navbar />
+                    <Routing />
+                </>
+            ) : (
+                <>
+                    <Login user={user} />
+                </>
+            )}
         </div>
-    )
+    );
 }
 
 export default App;
-
-//function App() {
-//    const [user, setUser] = useState(null);
-
-//    useEffect(() => {
-//        firebase.auth().onAuthStateChanged((authed) => {
-//            console.log(authed)
-//            if (authed) {
-//                const userName = authed.displayName;
-//                const values = userName.split(" ");
-//                const fName = values[0];
-//                const lName = values[1];
-//                const userInfoObj = {
-//                    id: authed.uid,
-//                    firstName: fName,
-//                    lastName: lName,
-//                    email: authed.email,
-//                };
-//                setUser(userInfoObj);
-
-
-
-
-//            } else if (user || user === null) {
-//                setUser(false);
-//            }
-//        });
-//    }, []);
-//    return (
-//        <div>
-//            {user ? (
-//                <>
-//                    <Navbar />
-//                    <Routing uid={user.id} />
-//                </>
-//            ) : (
-//                <Login user={user} />
-//            )}
-//        </div>
-//    );
-//}
-
-//export default App;
-
-
