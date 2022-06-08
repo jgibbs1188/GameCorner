f026248c124c67984f8291c262a4f203acd40ef7
Joe Gibbs <joseph.gibbs1188@gmail.com>
2022-06-07 21:09:47 -0500

    game & user promises written and tested in Postman

--- gamecornerclient/src/api/gameData.js
+++ gamecornerclient/src/api/gameData.js
@@ -1,1 +1,54 @@
-console.log("Hello World!")
+import axios from "axios";
+
+const baseURL = "https://localhost:7035/api"
+
+const getGameById = (id) =>
+  new Promise((resolve, reject) => {
+    axios
+      .get(`${baseURL}/Games/${id}`)
+      .then((response) => resolve(response.data))
+      .catch(reject);
+  });
+
+  const getGamesByUserId = (userId) =>
+  new Promise((resolve, reject) => {
+    axios
+      .get(`${baseURL}/Games/user/${userId}`)
+      .then((response) => resolve(Object.values(response.data)))
+      .catch(reject);
+  });
+
+  const createGame = (newGame) =>
+  new Promise((resolve, reject) => {
+    axios
+      .post(`${baseURL}/Games`, newGame)
+      .then((response) => {
+        resolve(response.data.id);
+      })
+      .catch(reject);
+  });
+
+  const updateGame = (gameObj) =>
+  new Promise((resolve, reject) => {
+    axios
+      .put(`${baseURL}/Games/${gameObj.id}`, gameObj)
+      .then(() =>getGamesByUserId(gameObj.userId).then(resolve))
+    
+      .catch(reject);
+  });
+
+  const deleteGame = (gameId, userId) =>
+  new Promise((resolve, reject) => {
+    axios
+      .delete(`${baseURL}/Games/${gameId}`)
+      .then(() => getGamesByUserId(userId).then(resolve))
+      .catch(reject);
+  });
+
+  export {
+      getGameById,
+      getGamesByUserId,
+      createGame,
+      updateGame,
+      deleteGame,
+  }
